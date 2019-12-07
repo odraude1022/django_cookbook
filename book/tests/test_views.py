5d15b6d7c49d4de5fb47277c6d6035209bd007c7
Eduardo Iglesias <eriglesias@ufl.edu>
2019-12-07 01:20:38 -0500

    add view to create new user

---
+++ book/tests/test_views.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from book.models import User, Recipe, Step, Ingredient
+import json
+
+new_user = {
+  "username": "hello123",
+  "email": "hello123@example.com",
+  "first_name": "Hello",
+  "last_name": "world"
+}
+
+class UserTest(TestCase):
+  def test_can_create_new_user(self):
+    response = self.client.post('/users/new', data=json.dumps(new_user), content_type="application/json")
+    self.assertContains(response, "hello123")
+    user = User.objects.first()
+    self.assertEquals(user.username, "hello123")
+
+  def test_get_error_when_post_with_invalid_data(self):
+    self.client.post('/users/new', data=json.dumps(new_user), content_type="application/json")
+
+    response = self.client.post('/users/new', data=json.dumps(new_user), content_type="application/json")
+    self.assertContains(response, "error")
--- book/views.py
+++ book/views.py
@@ -1,3 +1,22 @@
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from book.models import User
+import json
 
-# Create your views here.
+@csrf_exempt
+def new_user(request):
+  data = json.loads(request.body)
+  username = data.get('username')
+  email = data.get('email')
+  first_name = data.get('first_name')
+  last_name = data.get('last_name')
+
+  new_user = User(username=username, email=email, first_name=first_name, last_name=last_name)
+  try:
+    new_user.save()
+    serialized_data = serializers.serialize('json', [ new_user])  
+    return HttpResponse(serialized_data)
+  except Exception as e:
+    return JsonResponse({"error": str(e)})
